5cf9e43ddfbbcc57e9184b7c851a2d0e51b8da9b
taeri <lola161385@naver.com>
2025-01-03 11:52:15 +0900

    first commmit

---
+++ src/main/java/w1_1_grammar/Grammar.java
@@ -0,0 +1,27 @@
+package w1_1_grammar;
+
+/**
+ * 협업하는 개발자들을 위해서
+ * 작성한 프로그램 요소별로
+ * 친절한 Docs 주석을 작성할 수 있습니다.
+ * =>Class에 대한 Docs에 생성됨
+ */
+
+public class Grammar {
+    // 한줄 주석: 주석도 코드다!
+    /*
+        여러줄 주석
+     */
+
+    /**
+     * 메서드 Docs에 대해서는
+     * 작성하는 특정한 포맷이 존재함
+     * -> API 문서 작성 및 자동 생성에 필요한 기본 포맷이 됨
+     * @param args
+     *  :외부에서 문자열 입력을 수신하는 파라미터
+     */
+    public static void main(String[] args) {
+
+
+    }
+}
